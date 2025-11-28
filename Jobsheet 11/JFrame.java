684cc7f29443e85f6d57b95101fc19b5db364321
Andini Tribuana <threebuanandini@gmail.com>
2025-11-28 17:31:57 +0700

    learn GUI

---
+++ Jobsheet 11/JFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.JFrame;
+
+public class JFrame {
+    public static void main(String[] args) {
+        // membuat objek JFrame
+        JFrame frame = new JFrame("My app");
+
+        // membuat ukuran jendela
+        frame.setSize(300, 200);
+
+        // menutup apk ketika di close
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // menampilkan jendela
+        frame.setVisible(true);
+    }
+}
